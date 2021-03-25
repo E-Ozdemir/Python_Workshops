72806b8ba4f5bf89b7c8368231d2b31ada3a8cf5
Emre Özdemir <55084899+E-Ozdemir@users.noreply.github.com>
2021-03-25 16:56:54 +0100

    Add files via upload

---
+++ Problem_1.py
@@ -0,0 +1,6 @@
+# Problem 1
+# Write a program that multiplies three numbers entered by the user. Print the output.
+x = int(input('Write first number to multiple: '))
+y = int(input('Write second number to multiple: '))
+z = int(input('Write thirt number to multiple: '))
+print('Equals:',(x* y* z))
